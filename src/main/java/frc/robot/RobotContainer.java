8c1b1a5a671d6f1378bd497f0f2bf0bde1867b8a
Clayton <Claynderf22@gmail.com>
2025-01-18 14:04:04 -0500

    finished the drive mechanism

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,13 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-//import frc.robot.commands.Autos;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+//import frc.robot.commands.Autos;
+import frc.robot.subsystems.TankDrive;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,13 +25,18 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
+       TankDrive m_tankdrive = new TankDrive();
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+     m_tankdrive = new TankDrive();
     // Configure the trigger bindings
     configureBindings();
   }
-
+public void teleopPeriodic() {
+  m_tankdrive.drive(-m_driverController.getLeftY(), -m_driverController.getRightY());
+  
+    
+}
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -1,14 +1,45 @@
 package frc.robot.subsystems;
-
-// imported motor id numbers
+//imported motor ids
 import static frc.robot.Constants.DriveConstants.KleftfrontID;
 import static frc.robot.Constants.DriveConstants.kleftbackID;
-import static frc.robot.Constants.DriveConstants.krightfrontID;
 import static frc.robot.Constants.DriveConstants.krightbackID;
+import static frc.robot.Constants.DriveConstants.krightfrontID;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 
 // imported victor library
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-public class TankDrive {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TankDrive extends SubsystemBase { 
+    
+    WPI_VictorSPX m_leftfront;
+    WPI_VictorSPX m_leftback;
+    WPI_VictorSPX m_rightfront;
+    WPI_VictorSPX m_rightback;
+    DifferentialDrive m_tankDrive;
+
+    public TankDrive() {
+
+        //setting up the motors
+        m_leftfront = new WPI_VictorSPX(KleftfrontID);
+        m_leftback = new WPI_VictorSPX(kleftbackID);
+        m_rightfront = new WPI_VictorSPX(krightfrontID);
+        m_rightback = new WPI_VictorSPX(krightbackID);
+         
+        m_rightfront.setInverted(true);
+        m_rightback.setInverted(true);
+        m_rightback.follow(m_rightfront);
+        m_leftback.follow(m_leftfront);
+        
+    
+        m_tankDrive = new DifferentialDrive(m_leftfront, m_rightfront);
+}
+
+    public void drive(double leftSpeed, double rightSpeed) {
+        m_tankDrive.tankDrive(leftSpeed, rightSpeed);
+    }
+
     
 }
