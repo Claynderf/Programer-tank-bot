297216cf61634cea89893d3ceb492e35649b9ade
Clayton <Claynderf22@gmail.com>
2025-01-18 11:26:37 -0500

    imported librarys and made constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,10 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class DriveConstants {
+    public static final int KleftfrontID = 0;
+    public static final int kleftbackID = 1;
+    public static final int krightfrontID = 2;
+    public static final int krightbackID = 3;
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+// imported motor id numbers
+import static frc.robot.Constants.DriveConstants.KleftfrontID;
+import static frc.robot.Constants.DriveConstants.kleftbackID;
+import static frc.robot.Constants.DriveConstants.krightfrontID;
+import static frc.robot.Constants.DriveConstants.krightbackID;
+
+// imported victor library
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class TankDrive {
+    
+}
